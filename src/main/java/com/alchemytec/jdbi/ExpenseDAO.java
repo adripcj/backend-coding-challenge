63eb35e07fe7cd6b327bb68dd7ce0261b8a2d9f7
Adrian Parisi <aparisi@kiusys.comm>
2015-11-05 16:46:12 -0300

    Removing method not used

--- src/main/java/com/alchemytec/jdbi/ExpenseDAO.java
+++ src/main/java/com/alchemytec/jdbi/ExpenseDAO.java
@@ -14,13 +14,6 @@ import com.alchemytec.core.ExpenseMapper;
 @RegisterMapper(ExpenseMapper.class)
 public interface ExpenseDAO {
 
-    @SqlUpdate("create table expense "
-            + "(id int primary key not null auto_increment,"
-            + " amount decimal(10,2),"
-            + " date date,"
-            + " reason varchar(100))")
-    void createExpenseTable();
-
     @SqlUpdate("insert into expense (amount, date, reason)"
             + " values (:amount, :date, :reason)")
     void insert(@Bind("amount") Double amount, @Bind("date") Date date, 
